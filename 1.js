370af57a2f5e44be4d226619bbe2c753852fd3c9
thahzi <thahzeebmohamed@gmail.com>
2024-06-08 13:44:55 +0530

    initial

---
+++ 1.js
@@ -0,0 +1,80 @@
+//js is dinamicly  typed language
+
+//data types= are two types
+// primitive typs =
+// String
+// Number
+// Boolean
+// bigInt no.s larger than 2**53
+// undefined not given
+// null no valur
+// Symbol unique identifier
+
+// non-primitive =
+//Object vlues are stored as key:value pair
+
+
+// console.log(typeof('mt'));
+// console.log(typeof('50,44.5'));
+// let log = true // booean
+// console.log(typeof{log});
+// let x = null //null
+// console.log(x); 
+
+//variables
+//used to store data
+
+//Syntax
+// variables_name = vaule 
+// (x=10 
+// console.log(x);)
+
+// keyword variable_name =value 
+// 1 const reassaining not possible
+// 2 var reassaning is possible
+// 3 let
+
+
+// x=10
+// console.log(typeof(x));
+
+//    prog4 keywords
+// const x = 10
+// console.log(x);
+
+// var x=10
+// x=20
+// console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//objects
+// let course ={
+//     name : "mt",
+//     class : 3,
+// };
+// console.log(course.class);
+// course.name='tm';
+// console.log(course.name);
+
+//array
+// let color=['bule','red'];
+// console.log(color[1]);
+// console.log(color.length);
+// let a=10
+// a= a+1
+// a=a-2
+// a=a++
+// console.log(a);
