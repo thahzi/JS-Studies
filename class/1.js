b4da27ef3337ac0e1814c53bf53e06c76821dc00
thahzi <thahzeebmohamed@gmail.com>
2024-08-02 22:44:23 +0530

    aug2

---
+++ class/1.js
@@ -0,0 +1,21 @@
+//string
+var person = "john" ;
+let person1 = "smith";
+
+//number
+var age = 25 ;
+let age1 = 25 ;
+
+//boolean
+var js =true;
+var html = false;
+
+//undefined
+
+var text1;
+
+//null
+var text2 = null;
+
+var a=10, b=20;
+console.log(a+b);
